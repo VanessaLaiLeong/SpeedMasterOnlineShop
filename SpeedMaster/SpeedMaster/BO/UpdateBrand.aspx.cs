78af7bfbed1a7e48d4740c7f62ab8debc6daa2bb
Gustavo Meireles <gugatpm@gmail.com>
2023-12-07 00:24:11 +0000

    Brand update, delete and show done

--- SpeedMaster/SpeedMaster/BO/UpdateBrand.aspx.cs
+++ SpeedMaster/SpeedMaster/BO/UpdateBrand.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,35 @@ namespace SpeedMaster.BO
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["brand_id"] != null)
+                {
+                    int brand_id = Convert.ToInt16(Request.QueryString["brand_id"]);
+                    string brand_name = Connections.getBrandById(brand_id);
+                    string country_of_origin = Connections.getBrandCountryOriginById(brand_id);
 
+                    tb_brand_name.Text = brand_name;
+                    tb_brand_origin.Text = country_of_origin;
+                }
+            }
+        }
+
+        protected void btn_submit_Click(object sender, EventArgs e)
+        {
+            int brand_id = Convert.ToInt32(Request.QueryString["brand_id"]);
+            string brand_name = tb_brand_name.Text;
+            string country_of_origin = tb_brand_origin.Text;
+
+            Connections.UpdateBrandInDB(brand_id, brand_name, country_of_origin);
+            Response.Redirect("ShowAllBrands.aspx");
+        }
+
+        protected void btn_delete_Click(object sender, EventArgs e)
+        {
+            int brand_id = Convert.ToInt32(Request.QueryString["brand_id"]);
+            Connections.DeleteBrandFromDB(brand_id);
+            Response.Redirect("ShowAllBrands.aspx");
         }
     }
 }
