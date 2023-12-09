97d0b51bf051560f105803501632f944401c00f9
Gustavo Meireles <gugatpm@gmail.com>
2023-12-09 02:52:44 +0000

    Page to view all existing orders

---
+++ SpeedMaster/SpeedMaster/BO/ShowAllOrders.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SpeedMaster.BO
+{
+    public partial class ShowAllOrders : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void rp_orders_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            DataRowView dr = (DataRowView)e.Item.DataItem;
+            ((Label)e.Item.FindControl("lbl_idOrder")).Text = dr["ID_Order"].ToString();
+            ((Label)e.Item.FindControl("lbl_orderDate")).Text = dr["OrderDate"].ToString();
+            ((Label)e.Item.FindControl("lbl_shippingDate")).Text = dr["ShippingDate"].ToString();
+            ((Label)e.Item.FindControl("lbl_totalAmount")).Text = dr["TotalAmount"].ToString();
+            ((Label)e.Item.FindControl("lbl_statusName")).Text = dr["StatusName"].ToString();
+        }
+    }
+}
