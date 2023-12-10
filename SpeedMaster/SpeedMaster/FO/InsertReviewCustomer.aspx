dc2209ab83553e05825327d66d6cc6d4054eb62d
VanessaLaiLeong <118760958+VanessaLaiLeong@users.noreply.github.com>
2023-12-10 20:32:59 +0000

    review

--- SpeedMaster/SpeedMaster/FO/InsertReviewCustomer.aspx
+++ SpeedMaster/SpeedMaster/FO/InsertReviewCustomer.aspx
@@ -3,11 +3,14 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-   <asp:LinkButton ID="star1" runat="server" ForeColor="#FF9933" OnClick="star1_Click" CssClass="fa-regular fa-star no-decoration" style="text-decoration: none;"></asp:LinkButton>
-    <asp:LinkButton ID="LinkButton1" runat="server" ForeColor="#FF9933" OnClick="star1_Click" CssClass="fa-regular fa-star no-decoration"></asp:LinkButton>
+
+    <asp:LinkButton ID="star1" runat="server" ForeColor="#FF9933" OnClick="star1_Click" CssClass="fa-regular fa-star no-decoration" CommandArgument="1" style="text-decoration: none;"></asp:LinkButton>
+    <asp:LinkButton ID="star2" runat="server" ForeColor="#FF9933" OnClick="star2_Click" CssClass="fa-regular fa-star no-decoration" CommandArgument="2" style="text-decoration: none;"></asp:LinkButton>
+    <asp:LinkButton ID="star3" runat="server" ForeColor="#FF9933" OnClick="star3_Click" CssClass="fa-regular fa-star no-decoration" CommandArgument="3" style="text-decoration: none;"></asp:LinkButton>
+    <asp:LinkButton ID="star4" runat="server" ForeColor="#FF9933" OnClick="star4_Click" CssClass="fa-regular fa-star no-decoration" CommandArgument="4" style="text-decoration: none;"></asp:LinkButton>
+    <asp:LinkButton ID="star5" runat="server" ForeColor="#FF9933" OnClick="star5_Click1" CssClass="fa-regular fa-star no-decoration" CommandArgument="5" style="text-decoration: none;"></asp:LinkButton>
 
 
 
-   
 
 </asp:Content>
--- SpeedMaster/SpeedMaster/FO/InsertReviewCustomer.aspx.cs
+++ SpeedMaster/SpeedMaster/FO/InsertReviewCustomer.aspx.cs
@@ -16,7 +16,49 @@ namespace SpeedMaster.FO
 
         protected void star1_Click(object sender, EventArgs e)
         {
-            
+            star1.CssClass = "fa-solid fa-star";
+            star2.CssClass = "fa-regular fa-star";
+            star3.CssClass = "fa-regular fa-star";
+            star4.CssClass = "fa-regular fa-star";
+            star5.CssClass = "fa-regular fa-star";
+        }
+
+       
+
+        protected void star2_Click(object sender, EventArgs e)
+        {
+            star1.CssClass = "fa-solid fa-star";
+            star2.CssClass = "fa-solid fa-star";
+            star3.CssClass = "fa-regular fa-star";
+            star4.CssClass = "fa-regular fa-star";
+            star5.CssClass = "fa-regular fa-star";
+        }
+
+        protected void star3_Click(object sender, EventArgs e)
+        {
+            star1.CssClass = "fa-solid fa-star";
+            star2.CssClass = "fa-solid fa-star";
+            star3.CssClass = "fa-solid fa-star";
+            star4.CssClass = "fa-regular fa-star";
+            star5.CssClass = "fa-regular fa-star";
+        }
+
+        protected void star4_Click(object sender, EventArgs e)
+        {
+            star1.CssClass = "fa-solid fa-star";
+            star2.CssClass = "fa-solid fa-star";
+            star3.CssClass = "fa-solid fa-star";
+            star4.CssClass = "fa-solid fa-star";
+            star5.CssClass = "fa-regular fa-star";
+        }
+
+        protected void star5_Click1(object sender, EventArgs e)
+        {
+            star1.CssClass = "fa-solid fa-star";
+            star2.CssClass = "fa-solid fa-star";
+            star3.CssClass = "fa-solid fa-star";
+            star4.CssClass = "fa-solid fa-star";
+            star5.CssClass = "fa-solid fa-star";
         }
     }
 }
