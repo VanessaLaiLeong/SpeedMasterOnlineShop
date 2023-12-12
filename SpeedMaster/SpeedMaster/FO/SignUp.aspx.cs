bc299c7210112a57417ff562db94bca36f99de27
VanessaLaiLeong <118760958+VanessaLaiLeong@users.noreply.github.com>
2023-12-12 14:17:56 +0000

    pesquenos fix

--- SpeedMaster/SpeedMaster/FO/SignUp.aspx.cs
+++ SpeedMaster/SpeedMaster/FO/SignUp.aspx.cs
@@ -27,10 +27,27 @@ namespace SpeedMaster.FO
                 tb_address.Text = "";
             }
             string result = Services.doCreateCustomer(tb_email.Text, tb_password.Text, tb_name.Text, tb_surname.Text, tb_phone.Text, tb_address.Text, tb_nif.Text, tb_otherPassword.Text);
-            lbl_result.Text = result;
-            lbl_result.Visible = true;
-            Session["message"] = result;
-            Response.Redirect("checkEmail.aspx");
+            if (result == "Password does not match")
+            {
+                lbl_result.Text = result;
+                lbl_result.Visible = true;
+            }
+            else if (result == "Please insert a stronger password")
+            {
+                lbl_result.Text = result;
+                lbl_result.Visible = true;
+            }
+            else if (result == "Email already exists")
+            {
+                lbl_result.Text = result;
+                lbl_result.Visible = true;
+            }    
+            else
+            {
+                Session["message"] = result;
+                Response.Redirect("checkEmail.aspx");
+            }           
+            
         }
     }
 }
